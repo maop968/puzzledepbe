7fed3d3c400129ab76b011d08813adb77cf0985c
maop <maoping968@hotmail.com>
2021-03-08 19:52:52 +0100

    puzzle 1

---
+++ PUZZLE1.py
@@ -0,0 +1,26 @@
+from pynfc import Nfc, Desfire, Timeout
+
+n = Nfc("pn532_uart:/dev/ttyS0:115200")
+
+
+
+
+
+
+
+class Rfid:
+    def read_uid532(self):
+        for target in n.poll():
+            try:
+                targetaid = target.uid.decode("ascii").upper()
+                return targetaid
+            except TimeoutException:
+                pass
+        
+        
+if __name__=="__main__":
+     rf=Rfid()
+     uid=rf.read_uid532()
+     print(uid)
+    
+

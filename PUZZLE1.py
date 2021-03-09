deb69e433cc2d42a2a4d5c774796668b5d4ebaee
maop968 <78744469+maop968@users.noreply.github.com>
2021-03-09 20:10:32 +0100

    Update PUZZLE1.py

--- PUZZLE1.py
+++ PUZZLE1.py
@@ -3,11 +3,6 @@ from pynfc import Nfc, Desfire, Timeout
 n = Nfc("pn532_uart:/dev/ttyS0:115200")
 
 
-
-
-
-
-
 class Rfid:
     def read_uid532(self):
         for target in n.poll():
@@ -18,9 +13,9 @@ class Rfid:
                 pass
         
         
-if __name__=="__main__":
-     rf=Rfid()
-     uid=rf.read_uid532()
+if __name__ == "__main__":
+     rf = Rfid()
+     uid = rf.read_uid532()
      print(uid)
     
 

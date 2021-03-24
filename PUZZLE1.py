46bf1d475679465ff2e8e5cb898197be44cdda81
maop <maoping968@hotmail.com>
2021-03-24 11:01:36 +0100

    puzzle2 dia 2

--- PUZZLE1.py
+++ PUZZLE1.py
@@ -1,26 +1,22 @@
 from pynfc import Nfc, Desfire, Timeout
 
-n = Nfc("pn532_uart:/dev/ttyS0:115200")
-
-
-
-
-
-
-
 class Rfid:
+
+    def __init__(self):
+            n = Nfc("pn532_uart:/dev/ttyS0:115200")
+
     def read_uid532(self):
         for target in n.poll():
-            try:
+            try:               
                 targetaid = target.uid.decode("ascii").upper()
                 return targetaid
             except TimeoutException:
                 pass
         
         
-if __name__=="__main__":
-     rf=Rfid()
-     uid=rf.read_uid532()
+if __name__ == "__main__":
+     rf = Rfid()
+     uid = rf.read_uid532()
      print(uid)
     
 
